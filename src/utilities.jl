6159f0a17e4a50040dfafe5e716605c8bf13b670
Tim Besard <tim.besard@gmail.com>
2023-07-10 19:57:14 +0200

    Add profile macro.

--- src/utilities.jl
+++ src/utilities.jl
@@ -1,5 +1,15 @@
 # high-level utilities
 
+macro profile(ex)
+    quote
+        isactive() || @warn("No ITT collector present. Make sure you have a collector attached to your process, e.g., by running under VTune.")
+        resume()
+        # Use Expr(:tryfinally, ...) so we don't introduce a new soft scope.
+        # TODO: switch to solution once https://github.com/JuliaLang/julia/pull/39217 is resolved
+        $(Expr(:tryfinally, esc(ex), :(pause())))
+    end
+end
+
 macro range(ex...)
     length(ex) >= 2 || error("Usage: IntelITT.@range [domain=...] name::String code")
     name = ex[end-1]
